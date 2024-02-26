37532ead3e736ea05fc08e9a0bac247292321ee0
Thaumagraph <159125111+Thaumagraph@users.noreply.github.com>
2024-02-26 08:40:43 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// switch structure code-along
+
+let myValue = 100;
+
+switch (myValue) {
+  case 50:
+    console.log("var er 50");
+    break;
+
+  default:
+    console.log("default");
+    break;
+}
